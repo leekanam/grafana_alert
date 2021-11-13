527788caae55d90061834748b1fc483d4ac1d2a2
leekanam <leekanam20001111@gmail.com>
2021-11-13 11:06:10 +0800

    u

---
+++ dump/myDb.sql
@@ -0,0 +1,12 @@
+CREATE USER '21033593d'@'%' IDENTIFIED BY '12345';
+GRANT ALL PRIVILEGES ON * . * TO '21033593d' WITH GRANT OPTION;
+
+SELECT * FROM mysql.user;
+SELECT user FROM mysql.user;
+SELECT user.host FROM mysql.user;
+SELECT * FROM mysql.user;
+SELECT user FROM mysql.user;
+SELECT user.host FROM mysql.user;
+SELECT * FROM mysql.user;
+SELECT user FROM mysql.user;
+SELECT user.host FROM mysql.user;
